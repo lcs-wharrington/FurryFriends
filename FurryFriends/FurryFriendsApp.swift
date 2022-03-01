720fa679834b962e2ddfc6702396e8eb48bbcd4f
William Harrington <wharrington@lcs.on.ca>
2022-03-01 09:23:09 -0500

    Initial Commit

---
+++ FurryFriends/FurryFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurryFriendsApp.swift
+//  FurryFriends
+//
+//  Created by William Robert Harrington on 2022-03-01.
+//
+
+import SwiftUI
+
+@main
+struct FurryFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
